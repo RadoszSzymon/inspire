b70f32f42cabde262c922ef29da936ae59fca51c
RadoszSzymon <radoszszymon@gmail.com>
2016-06-24 16:26:01 +0200

    edit and delete posts actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :upate, :destroy]
 
   def index
+    #zmienna @posts zawiera wszystkie posty ulozone od najnowszego
     @posts = Post.all.order("created_at DESC")
   end
 
@@ -13,10 +14,13 @@ class PostsController < ApplicationController
   end
 
   def create
+    #tworze nowy post przypisany do zmiennej @post z post_params
     @post = Post.new(post_params)
 
+    #jesli uda sie zapisac(wszystko bedzie ok) to przechodze do tego postu
     if @post.save
       redirect_to @post
+    #jesli nie to renderuje od nowa formularz new
     else
       render 'new'
     end
@@ -26,14 +30,24 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    #usuwam post i prenosze sie na strone glowna
+    @post.destroy
+    redirect_to root_path
   end
 
   private
 
   def find_post
+    #przypisuje do zmiennej @post znaleziony post o konkretnym id
     @post = Post.find(params[:id])
   end
 
