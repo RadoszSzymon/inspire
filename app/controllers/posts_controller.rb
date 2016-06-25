3b29c327ac9cc0e1e3dade7e8451d986a6b75169
RadoszSzymon <radoszszymon@gmail.com>
2016-06-25 07:16:04 +0200

    added images to posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comments = Comment.where(post_id: @post)
   end
 
   def new
@@ -53,6 +54,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :link, :description)
+    params.require(:post).permit(:title, :link, :description, :image)
   end
 end
